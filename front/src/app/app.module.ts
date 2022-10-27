265f322b9780c412c232019807c569ae6e464681
Daniel Garzón <dgarzonc120@gmail.com>
2022-10-26 20:45:03 -0500

    Components + Styles
    Se crean los componentes a usar y se agregan estilos con Bootstrap

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HeroFormComponent } from './components/hero-form/hero-form.component';
+import { HeroListComponent } from './components/hero-list/hero-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeroFormComponent,
+    HeroListComponent
   ],
   imports: [
     BrowserModule,
