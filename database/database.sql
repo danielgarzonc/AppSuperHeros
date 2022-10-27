b2f4a5e135c9d191e44ec05873787abb5202be28
Daniel Garzón <dgarzonc120@gmail.com>
2022-10-27 13:33:20 -0500

    Actualizacion Consulta SQL

--- database/database.sql
+++ database/database.sql
@@ -27,7 +27,7 @@ CREATE TABLE vehicles(
     description VARCHAR(255)
 );
 
-
-DESCRIBE heros;
-DESCRIBE power_types;
-DESCRIBE vehicles;
+INSERT INTO heros (name, hero_type, operation_place, condicion, power_type, vehicle, image) VALUES ("Bruja Escarlata", "Heroe", "Ciudad", "Libertad", "Puede manejar la magia del caos, lo cual hace que su nivel de poder sea muy variable", "Capa", "https://i.blogs.es/e791d0/screenshot_1279/1366_2000.jpeg");
+INSERT INTO heros (name, hero_type, operation_place, condicion, power_type, vehicle, image) VALUES ("Dr. Extraño", "Heroe", "Ciudad", "Libertad", "Experto cirujano. Maestro con las artes de la magia y poseedor de una amplia colección de artefactos místicos.", "Capa", "https://i.blogs.es/8379ae/screenshot_1282/1366_2000.jpeg");
+INSERT INTO heros (name, hero_type, operation_place, condicion, power_type, vehicle, image) VALUES ("Mercurio (Quicksilver)", "Heroe", "Ciudad", "Libertad", "Su velocidad le permite correr más rápido que la luz e incluso atravesar objetos a nivel molecular.", "Ninguno", "https://i.blogs.es/595f98/screenshot_1290/1366_2000.jpeg");
+INSERT INTO heros (name, hero_type, operation_place, condicion, power_type, vehicle, image) VALUES ("Agente Venom", "Villano", "Ciudad", "Detenido", "fuerza, agilidad, velocidad y resistencia superhumanas.", "Ninguno", "https://i.blogs.es/2306ee/agente-venom/1366_2000.jpeg");
